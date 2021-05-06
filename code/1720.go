af9d9ff3e36d2c39040e8fcaae63636e0057f551
wenqiq <wenqiq@vmware.com>
2021-05-06 23:14:35 +0800

    add daily code practice: 1720. Decode XORed Array;

---
+++ code/1720.go
@@ -0,0 +1,15 @@
+package test
+
+func decode(encoded []int, first int) []int {
+
+	res := make([]int, len(encoded)+1)
+
+	res[0] = first
+
+	for i, num := range encoded {
+		res[i+1] = res[i] ^ num
+	}
+
+	return res
+
+}
